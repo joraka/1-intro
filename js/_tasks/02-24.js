c734300e27429ba7338244a41cb9ed59ede1763c
Jonas <92853498+joraka@users.noreply.github.com>
2025-02-26 16:51:34 +0200

    02-26b

--- js/_tasks/02-24.js
+++ js/_tasks/02-24.js
@@ -1,46 +1,71 @@
 // https://github.com/bit-challenges/shopping-cart-challenge
 
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
+
+const rl = readline.createInterface({ input, output });
+
 const store = [
   { id: "1", name: "🥕 Carrot", price: 2, stock: 10 },
   { id: "2", name: "🍅 Tomato", price: 3, stock: 8 },
   { id: "3", name: "🥦 Broccoli", price: 4, stock: 5 },
 ];
 
-const cart = []; // Empty shopping cart
+const cart = [];
+
+function validateId(id) {
+  if (typeof id !== "number" || isNaN(id) || id % 1 !== 0 || id <= 0)
+    return console.log("invalid id entered");
+  return true;
+}
+
+function validateQuantity(quantity) {
+  if (typeof quantity !== "number" || isNaN(quantity) || quantity % 1 !== 0 || quantity <= 0)
+    return console.log(`invalid quantity entered, must be at least 1`);
+  return true;
+}
 
 // Adds product to the cart
 function addProduct(id, quantity) {
-  const productInStore = store.find((item) => item.id === id);
-  if (!productInStore) return console.log(`product not found in the store (id: ${id})`);
+  if (!validateId(id)) return;
+  if (!validateQuantity(quantity)) return;
+
+  const productInStore = store.find((item) => item.id === String(id));
+  if (!productInStore) return console.log(`product with id ${id} doesn't exists in the store`);
 
   if (quantity > productInStore.stock)
     return console.log(
-      `store doesn't have enough '${productInStore.name}', inventory is ${productInStore.stock}`
+      `store doesn't have enough products, available stock is ${productInStore.stock}`
     );
 
-  const isProductInCart = cart.some((item) => item?.product?.id === id);
+  const isProductInCart = cart.some((item) => item?.product?.id === String(id));
   if (isProductInCart)
     return console.log(
-      `cannot add product '${productInStore.name}' (id: ${productInStore.id}), it's already in the cart`
+      `cannot add product '${productInStore.name}' (id: ${productInStore.id}), it's already in the cart. Use 'update [item id] [quantity]' instead.`
     );
 
   cart.push({ product: productInStore, quantity });
   productInStore.stock -= quantity;
   console.log(
-    `${quantity} pieces of product '${productInStore.name}' (id: ${productInStore.id}) added`
+    `${quantity}x '${productInStore.name}' (id: ${productInStore.id}) added to the cart`
   );
 }
 
 // Removes a product from the cart
 function removeProduct(id) {
-  id = String(id);
-  if (isNaN(id) || id % 1 !== 0) return console.log("invalid id entered");
-  const productInCartIndex = cart.findIndex((item) => item?.product?.id === id);
+  if (!validateId(id)) return;
+
+  const productInCartIndex = cart.findIndex((item) => item?.product?.id === String(id));
+
   if (productInCartIndex === -1)
     return console.log(`there is no such product in the cart (id: ${id}) in the cart to remove`);
+
   const productInCart = cart[productInCartIndex];
+
   productInCart.product.stock += productInCart.quantity;
+
   cart.splice(productInCartIndex, 1);
+
   console.log(
     `product '${productInCart.product.name}' (id: ${productInCart.product.id}) removed from the cart`
   );
@@ -48,23 +73,19 @@ function removeProduct(id) {
 
 // Updates product quantity in the cart
 function updateQuantity(id, quantity) {
-  id = String(id);
-  if (isNaN(id) || id % 1 !== 0) return console.log("invalid id entered");
-  if (typeof quantity !== "number" || isNaN(quantity) || quantity % 1 !== 0)
-    return console.log("invalid quantity entered");
-  if (quantity < 0) return console.log("can't update quanity because it's less than 0");
+  if (!validateId(id)) return;
+  if (!validateQuantity(quantity)) return;
 
-  const productInCart = cart.find((item) => item?.product?.id === id);
+  const productInCart = cart.find((item) => item?.product?.id === String(id));
   if (!productInCart)
     return console.log(`there is no such product in the cart (id: ${id}) to update`);
 
-  const totalAvailableQuantity = productInCart.product.stock + productInCart.quantity;
-
   if (quantity === productInCart.quantity) return console.log(`cart already has ${quantity} items`);
 
+  const totalAvailableQuantity = productInCart.product.stock + productInCart.quantity;
   if (quantity > totalAvailableQuantity) {
     return console.log(
-      `store doesnt have that many products, current count is ${productInCart.quantity}`
+      `store doesnt have that many products, available stock is ${totalAvailableQuantity}`
     );
   } else {
     console.log(
@@ -79,47 +100,92 @@ function updateQuantity(id, quantity) {
 // Returns all cart items & total price
 function printCartDetails() {
   let totalCost = 0;
-  console.group("----[Cart Details]----");
-  for (const { product, quantity } of cart) {
-    const cost = quantity * product.price;
-    console.log(
-      `# id: ${product.id} | name: ${product.name} | quantity: ${quantity} | cost: ${Number(
-        cost
-      ).toFixed(2)} EUR`
-    );
-    totalCost += cost;
+  console.group("--------[Cart Details]--------");
+  if (cart.length > 0) {
+    for (const { product, quantity } of cart) {
+      const cost = quantity * product.price;
+      console.log(
+        `# id: ${product.id} | name: ${product.name} | quantity: ${quantity} | cost: €${Number(
+          cost
+        ).toFixed(2)}`
+      );
+      totalCost += cost;
+    }
+    console.log(`* Total price: €${Number(totalCost).toFixed(2).toLowerCase("lt")}`);
+  } else {
+    console.log("* There is no products in the cart");
   }
-  console.log(`[Total price: ${Number(totalCost).toFixed(2)} EUR]`);
   console.groupEnd();
-  console.log("-".repeat(22));
+  console.log("-".repeat(30));
 }
 
 function printStoreDetails() {
-  console.group("----[Store Details]----");
-  for (const { id, name, price, stock } of store) {
-    console.log(`# id: ${id} | name: ${name} | price: ${price} | stock: ${stock}`);
+  console.group("--------[Store Inventory]--------");
+  if (store.length > 0) {
+    for (const { id, name, price, stock } of store) {
+      console.log(`> id: ${id} | name: ${name} | price: €${price} | stock: ${stock}`);
+    }
+  } else {
+    console.log("* There is no products in the store");
   }
   console.groupEnd();
-  console.log("-".repeat(23));
+  console.log("-".repeat(33));
 }
 
-function startShopping() {} // (EXTRA) Interactive shopping experience
+// (EXTRA) Interactive shopping experience
+function startShoppingWindow() {
+  console.clear();
+  rl.question(
+    `Select options:\n` +
+      [
+        "> type 'store' for display store inventory",
+        "> type 'cart' for display cart inventory",
+        "> type 'add [item id] [quantity]' to add items to the cart",
+        "> type 'remove [item id]' to remove items from the cart",
+        "> type 'update [item id] [quantity]' to change the quantity in the cart",
+        "> type 'exit' to stop shopping",
+      ].join("\n") +
+      "\nEnter: ",
+    async (answer) => {
+      const [ans, itemId, quantity] = answer.split(" ");
+      switch (ans) {
+        case "store":
+          printStoreDetails();
+          break;
+        case "cart":
+          printCartDetails();
+          break;
+        case "add":
+          addProduct(Number(itemId), Number(quantity));
+          break;
+        case "remove":
+          removeProduct(Number(itemId));
+          break;
+        case "update":
+          updateQuantity(Number(itemId), Number(quantity));
+          break;
+        case "exit":
+          console.log("Exitting...");
+          process.exit(0);
+        default:
+          console.log("Invalid option entered, try again... ");
+          break;
+      }
 
-//---
-addProduct("1", 2);
-addProduct("1", 2);
-printStoreDetails();
-printCartDetails();
-updateQuantity("1", 4);
-printCartDetails();
-updateQuantity("1", -5);
-updateQuantity("1", 100);
-printCartDetails();
-printStoreDetails();
-updateQuantity("1", 10);
-printCartDetails();
-printStoreDetails();
-updateQuantity("1", 10);
-updateQuantity("1", 5);
-printCartDetails();
-printStoreDetails();
+      showPressEnterWindow();
+    }
+  );
+}
+
+function showPressEnterWindow() {
+  process.stdout.write("Press any key to continue ");
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  process.stdin.once("data", () => {
+    rl.clearLine(process.stdout);
+    process.stdin.setRawMode(false);
+    startShoppingWindow();
+  });
+}
+
+startShoppingWindow();
